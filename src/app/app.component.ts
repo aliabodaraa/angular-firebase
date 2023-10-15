f92a51bf677160900695c0f41ae3e3be96e94bcc
Ali Abodaraa <abodaraaali50@gmail.com>
2023-10-16 01:46:33 +0300

    Delete method to remove the course in fireBase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/compat/database';
-import { Observable } from 'rxjs';
+import { Observable, take } from 'rxjs';
 type fireBaseDataType = { name: string; price: number };
 @Component({
   selector: 'app-root',
@@ -29,5 +29,28 @@ export class AppComponent {
       .object<fireBaseDataType>('/courses/' + index)
       .update({ [named_property]: edited_input.value }); //update only the wanted property the property not mentioned here will not affected
     edited_input.value = '';
+  }
+
+  // deleteCourse(index: number) {      //failed in firebase you can't delete the object based on idex of array you must know the identifier in firebase
+  //   this.db
+  //     .object<fireBaseDataType>('/courses/' + index)
+  //     .remove()
+  //     .then(() => console.log('Deleted', index));
+  // }
+
+  deleteCourse(index: number) {
+    this.db
+      .list<fireBaseDataType>('/courses')
+      .snapshotChanges()
+      .pipe(take(1))
+      .subscribe((changes) => {
+        //we need to subscibe to take the key
+        const courseId = changes[index].payload.key;
+        this.db
+          .object(`/courses/${courseId}`)
+          .remove()
+          .then(() => console.log('Deleted', courseId))
+          .catch((error) => console.log('Error deleting', courseId, error));
+      });
   }
 }
